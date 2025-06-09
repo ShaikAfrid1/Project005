de786f7dbaaef242fdee8f2b16d9ae203e8c5709
afridshaik <shaikafrid500@gmail.com>
2025-06-09 13:59:23 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import axios from "./Api/AxiosConfig";
 const App = () => {
-  return <div>App</div>;
+  const getProduct = async () => {
+    try {
+      const res = await axios.get("/products");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+  return <div className="bg-black h-screen w-screen font-black text-white">App</div>;
 };
 
 export default App;
