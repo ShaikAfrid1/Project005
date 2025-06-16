03309ccb1acd76a77935c4f06cd6edf5b97a1b07
afridshaik <shaikafrid500@gmail.com>
2025-06-16 14:23:21 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import { asyncGetUsers } from "../src/store/UserActions";
-import { useDispatch, useSelector } from "react-redux";
-
 const App = () => {
-  const data = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  console.log(data);
-
-  useEffect(() => {
-    dispatch(asyncGetUsers());
-  }, [dispatch]);
-
   return (
-    <div className="bg-black h-screen w-screen font-black text-white">App</div>
+    <div className="bg-black h-screen w-screen font-black text-white">hi</div>
   );
 };
 
